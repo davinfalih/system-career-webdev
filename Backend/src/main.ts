1289baeca67db56cd2f9df280932da36916ad604
davinfalih <davinfalih628@gmail.com>
2026-08-18 21:02:27 +0700

    .

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -17,6 +18,15 @@ async function bootstrap() {
     credentials: true,
   });
   app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  const config = new DocumentBuilder()
+    .setTitle('JobMatch API')
+    .setDescription('The JobMatch API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   const port = Number(process.env.PORT ?? 4000);
   await app.listen(port);
